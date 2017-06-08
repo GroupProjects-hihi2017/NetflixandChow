c32644a0b30ea67abdcd2963bf540823a042ec95
jon-atto-bennett <jon.bennett@outlook.co.nz>
2017-06-08 16:46:38 +1200

    adding knock

--- tests/server/routes/movie.test.js
+++ tests/server/routes/movie.test.js
@@ -2,7 +2,7 @@
 var test = require('ava')
 var request = require('supertest')
 
-var createServer = require('../../server/server')
+// var createServer = require('../../server/server')
 
 var configureDatabase = require('./helpers/database-config')
 configureDatabase(test, createServer)
